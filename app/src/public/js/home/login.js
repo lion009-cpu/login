3965925a21fffa06b1ad88a19d244b0a27309104
Lion Zang <lion009@gmail.com>
2021-06-30 17:20:34 +0900

    supervisor로 구동하도록 변경

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -1,3 +1,15 @@
 "use strict";
 
-console.log("hello");
+const id = document.querySelector("#id"),
+      pwd= document.querySelector("#pwd"),
+      loginbtn= document.querySelector("button");
+
+loginbtn.addEventListener("click", login);
+
+function login() {
+    const req = {
+        id: id.value,
+        pwd: pwd.value,
+    };
+    console.log(req);
+}
